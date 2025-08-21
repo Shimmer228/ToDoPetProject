22f77bca2b0dc0f753a248aff9b1d34e043a6fd8
Rodion <rodson5973@gmail.com>
2025-08-21 12:24:22 +0300

    deploy fix3.5

--- ToDoAPI/app.js
+++ ToDoAPI/app.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const path = require('path');
 
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
+const app = express();
+
+// Middleware
 app.use(express.json());
 
+// CORS
 app.use(cors({
-  origin: 'https://todoapp-ma89.onrender.com',
+  origin: 'https://todoapp-ma89.onrender.com', // твій фронтенд
   methods: ['GET','POST','PATCH','DELETE'],
-  credentials: true
 }));
 
-app.get('/', (req, res) => {
-    res.send('API is working ✅');
-});
-
+// API маршрути
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
-app.use(express.static(path.join(__dirname, '../todo-client/build')));
 
+// Serve React frontend
+const buildPath = path.join(__dirname, '../todo-client/build');
+app.use(express.static(buildPath));
+
+// Для будь-якого іншого маршруту віддаємо index.html (React Router працює)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../todo-client/build', 'index.html'));
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
-module.exports = app;
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
