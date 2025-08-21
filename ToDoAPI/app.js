2a2657e755acaf3c34e88b85e333b190ccc31b70
Rodion <rodson5973@gmail.com>
2025-08-21 11:06:03 +0300

    deploy fix1

--- ToDoAPI/app.js
+++ ToDoAPI/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const cors = require('cors'); // <-- додаємо cors
 const app = express();
 
 const authRoutes = require('./routes/authRoutes');
-const taskRoutes = require('./routes/taskRoutes')
+const taskRoutes = require('./routes/taskRoutes');
+
 app.use(express.json());
+
+// Додаємо CORS
+app.use(cors({
+  origin: 'https://todoapp-ma89.onrender.com',
+  methods: ['GET','POST','PATCH','DELETE'],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send('API is working ✅');
 });
+
 app.use('/api/auth', authRoutes);
-app.use('/api/tasks',taskRoutes);
+app.use('/api/tasks', taskRoutes);
 
 module.exports = app;
