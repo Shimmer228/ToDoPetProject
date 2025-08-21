2fb2222bb554fc78b185d2ee76414687315e5c57
Rodion <rodson5973@gmail.com>
2025-08-21 13:14:43 +0300

    deploy fix4.2

--- todo-client/src/App.jsx
+++ todo-client/src/App.jsx
@@ -4,22 +4,29 @@ import LoginPage from './pages/LoginPage';
 import TodoPage from './pages/TodoPage';
 
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [token, setToken] = useState(localStorage.getItem('token')||null);
 
-  // можна ще ефект для синхронізації з localStorage
-  useEffect(() => {
-    const handleStorage = () => setToken(localStorage.getItem('token'));
-    window.addEventListener('storage', handleStorage);
-    return () => window.removeEventListener('storage', handleStorage);
-  }, []);
 
+ useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token);
+    } else {
+      localStorage.removeItem('token');
+    }
+  }, [token]);
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage setToken={setToken} />} />
-        <Route path="/" element={token ? <TodoPage /> : <Navigate to="/login" />} />
-      </Routes>
-    </Router>
+          <Routes>
+            <Route
+              path="/login"
+              element={token ? <Navigate to="/" /> : <LoginPage setToken={setToken} />}
+            />
+            <Route
+              path="/"
+              element={token ? <TodoPage /> : <Navigate to="/login" />}
+            />
+          </Routes>
+        </Router>
   );
 };
 
